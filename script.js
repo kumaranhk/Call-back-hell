a2cbb64e98044a29d8f5f07e0073e416edfa16e7
Kumaran <hariharakumaran007@gmail.com>
2024-02-24 15:53:24 +0530

    updated

--- script.js
+++ script.js
@@ -5,38 +5,42 @@ let happy = document.getElementById('happy');
 //Function for disabling the button 
 function disable(){
     btn.disabled = true;
-    btn.style.backgroundColor =
+    btn.style.boxShadow = '0px 0px 0px 0px rgb(80, 80, 80)';
+    btn.style.backgroundColor = '#ffcccc';
     setTimeout(() => {
         btn.disabled = false;
-    },11000);
+        btn.style.backgroundColor = 'rgb(184, 0, 0)';
+        btn.style.boxShadow = "2px 2px 2px 2px rgb(80, 80, 80)";
+    },12000);
 };
 
 //function for timer using call back hell
 function time(){
     setTimeout(() => {
-        timer.innerHTML = '10';
+        timer.style.fontSize = '45px';
+        timer.innerHTML = '00:00:10';
         setTimeout(() =>{
-            timer.innerHTML = '9';
+            timer.innerHTML = '00:00:09';
             setTimeout(() =>{
-                timer.innerHTML = '8';
+                timer.innerHTML = '00:00:08';
                 setTimeout(() =>{
-                    timer.innerHTML = '7';
+                    timer.innerHTML = '00:00:07';
                     setTimeout(() =>{
-                        timer.innerHTML = '6';
+                        timer.innerHTML = '00:00:06';
                         setTimeout(() =>{
-                            timer.innerHTML = '5';
+                            timer.innerHTML = '00:00:05';
                             setTimeout(() =>{
-                                timer.innerHTML = '4';
+                                timer.innerHTML = '00:00:04';
                                 setTimeout(() =>{
-                                    timer.innerHTML = '3';
+                                    timer.innerHTML = '00:00:03';
                                     setTimeout(() =>{
-                                        timer.innerHTML = '2';
+                                        timer.innerHTML = '00:00:02';
                                         setTimeout(() =>{
-                                            timer.innerHTML = '1';
+                                            timer.innerHTML = '00:00:01';
                                             setTimeout(() =>{
                                                 timer.style.fontSize = 'larger';
                                                 timer.innerHTML = 'Happy Independance Day!';
-                                            },1000);
+                                            },0);
                                         },1000);
                                     },1000);
                                 },1000);
